e5b42dafb2aa316fb85fde3fb8a9cc45fac57085
Haaris423111 <2010631170098@student.unsika.ac.id>
2022-10-20 20:55:44 +0700

    Menambahkan tugas praktikum 1

---
+++ nomor1.dart
@@ -0,0 +1,33 @@
+void main() {
+  int Integer = 89;
+
+  num Numb = 3.76;
+
+  double Double = 0.93;
+
+  String Strings = "Muhammad Haris";
+
+  bool Booleans = false;
+
+  const word = 'GGWP';
+  final Runess = word.runes.toList();
+
+  dynamic Dynamic = 25;
+  Dynamic = "Dua puluh lima";
+
+  Symbol Symbols = new Symbol("Ini adalah simbol");
+
+  String? Nulls; //
+
+  print(Integer);
+  print(Numb);
+  print(Double);
+  print(Strings);
+  print(Booleans);
+  print(word);
+  print(Runess);
+  print(Dynamic);
+  print(Symbols);
+  print(#IniSimbol);
+  print("$Nulls");
+}
